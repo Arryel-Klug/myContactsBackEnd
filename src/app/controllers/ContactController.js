e9e01f4cd259f229ef5b42dc868be2d2772aa49c
Arryel-Kauhu <arryel.klug@gmail.com>
2023-04-24 10:01:20 -0300

    Initial commit 24/04/2023

---
+++ src/app/controllers/ContactController.js
@@ -0,0 +1,116 @@
+const ContactRepository = require('../repositories/ContactRepository');
+const isValidUUID = require('../utils/isValidUUID');
+
+class ContactController {
+  async index(request, response) {
+    const { orderBy } = request.query;
+    const contacts = await ContactRepository.findAll(orderBy);
+
+    response.json(contacts);
+  }
+
+  async show(request, response) {
+    // Obter Um registro
+    const { id } = request.params;
+
+    if (!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid Contact ID' });
+    }
+
+    const contact = await ContactRepository.findById(id);
+
+    if (!contact) {
+      // 404 not found
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+    return response.json(contact);
+  }
+
+  async store(request, response) {
+    // criar novo registro
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    if (category_id && !isValidUUID(category_id)) {
+      return response.status(400).json({ error: 'Invalid category' });
+    }
+
+    if (email) {
+      const contactExists = await ContactRepository.findByEmail(email);
+
+      if (contactExists) {
+        return response.status(400).json({ error: 'This e-mail is already in use' });
+      }
+    }
+    const contact = await ContactRepository.create({
+      name,
+      email: email || null,
+      phone,
+      category_id: category_id || null,
+    });
+
+    response.status(201).json(contact);
+  }
+
+  async update(request, response) {
+    // editar um registro
+    const { id } = request.params;
+    const {
+      name, email, phone, category_id,
+    } = request.body;
+
+    if (!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid Contact ID' });
+    }
+
+    if (category_id && !isValidUUID(category_id)) {
+      return response.status(400).json({ error: 'Invalid category' });
+    }
+
+    if (!name) {
+      return response.status(400).json({ error: 'Name is required' });
+    }
+
+    const contactExists = await ContactRepository.findById(id);
+    if (!contactExists) {
+      return response.status(404).json({ error: 'Contact not found' });
+    }
+
+    if (email) {
+      const contactByEmail = await ContactRepository.findByEmail(email);
+
+      if (contactByEmail && contactByEmail.id !== id) {
+        return response.status(400).json({ error: 'This e-mail is already in use' });
+      }
+    }
+
+    const contact = await ContactRepository.update(id, {
+      name,
+      email: email || null,
+      phone,
+      category_id: category_id || null,
+    });
+
+    response.json(contact);
+  }
+
+  async delete(request, response) {
+    // deletar um registro
+    const { id } = request.params;
+
+    if (!isValidUUID(id)) {
+      return response.status(400).json({ error: 'Invalid Contacts ID' });
+    }
+
+    await ContactRepository.delete(id);
+    // 204: No content
+    response.sendStatus(204);
+  }
+}
+
+module.exports = new ContactController();
